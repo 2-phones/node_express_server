cc71cc045060d2d563e381402800b16f334bdeae
JoEunsoo <dcpg95@gmail.com>
2022-09-05 22:28:06 +0900

    패키지 json 수정

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 8081;
 
 app.use(cors());
 app.use(express.json({strict:false}));
 
-app.listen(8081,()=> {
-    console.log('서버 정상적으로 켜졌음');
+app.listen(PORT,()=> {
+    console.log(`서버 정상적으로 켜졌음 :  ${PORT}`);
 });
 
 app.get('/', (req, res) => {
